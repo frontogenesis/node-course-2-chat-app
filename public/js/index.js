d5448aaed6c686acbdbbb60d0c34cf3858ea830f
Ray Hawthorne <frontogenesis@gmail.com>
2018-03-21 11:44:26 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Ray',
-        message: 'Message from client'
-    });
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(msg) {
-    console.log('New Message', msg);
+socket.on('newMessage', function(message) {
+    console.log('New Message', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,19 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'ray@ray.com',
-        text: 'This is a test',
-        createAt: 123456
-    });
-
-    socket.emit('newMessage', {
-        message: 'here is the message',
-        createdAt: 35954
-    });
-
-    socket.on('createMessage', (msg) => {
-        console.log('createMessage', msg);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', (socket) => {
