6a1bb968346edce23e9f100bb1d33712e6523fa5
Ray Hawthorne <frontogenesis@gmail.com>
2018-03-21 19:35:08 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -11,8 +11,23 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// socket.emit emits an event to a single connection
+// io.emit emits an event to every connection
+
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -21,6 +36,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', (socket) => {
